d66a4dbda45c4b11d966eba9411389e40f859905
diimon <115620359+diimon04@users.noreply.github.com>
2026-01-13 20:20:21 +0100

    updated picking random card and added mintBatch

--- smart-contracts/TCG1155.sol
+++ smart-contracts/TCG1155.sol
@@ -12,7 +12,7 @@ contract TCG1155 is ERC1155, Ownable {
     // Dynamic list of all card IDs for boosters
     uint256[] public cardIds;
 
-    constructor() ERC1155("https://example.com/metadata/{id}.json") Ownable(msg.sender) {
+    constructor() ERC1155("https://example.com/metadata/{id}.json") Ownable(msg.sender) { // has to be changed later with local uri for metadata
         // Add initial cards
         _addCardType(1, 20); // Pikachu Standard
         _addCardType(2, 3);  // Pikachu VMAX
@@ -40,21 +40,49 @@ contract TCG1155 is ERC1155, Ownable {
 
     // Open booster pack (variable size)
     function openBooster(uint256 packSize) public {
-        require(packSize > 0, "Pack size must be > 0");
+        require(packSize > 0 && packSize <= 10, "Pack size too big"); // limit of pack size set to 10
+
+        // arrays for batch minting
+        uint256[] memory idsToMint = new uint256[](packSize);
+        uint256[] memory amounts = new uint256[](packSize);
 
         for (uint256 i = 0; i < packSize; i++) {
-            uint256 cardType = _randomCard();
-            require(totalMinted[cardType] < maxSupply[cardType], "No more cards of this type");
+            uint256 cardType = _pickRandomAvailableCard();
+
+            require(cardType != 0, "No cards are available");
+
             totalMinted[cardType] += 1;
-            _mint(msg.sender, cardType, 1, "");
+
+            idsToMint[i] = cardType;
+            amounts[i] = 1;
         }
+
+        _mintBatch(msg.sender, idsToMint, amounts, ""); // saves gas cost if done with _mintBatch
     }
 
-    // Fake randomness for MVP
-    function _randomCard() internal view returns (uint256) {
-        require(cardIds.length > 0, "No cards available");
-        uint256 rand = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender)));
-        return cardIds[rand % cardIds.length];
+    function _pickRandomAvailableCard() internal view returns (uint256) {
+        if (cardIds.length == 0) return 0;
+
+        uint256 rand = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender))); // newer version: prevrandao instead of difficulty
+        
+        for(uint256 i = 0; i < 10; i++) {
+            uint256 index = (rand + i) % cardIds.length;
+            uint256 id = cardIds[index];
+
+            if (totalMinted[id] < maxSupply[id]) { // if card got minted 3 times its not available anymore
+                return id;
+            }
+        }
+
+        // if the for-loop above does not find a suitable cardtype, it gets choosen linear here
+        for (uint256 i = 0; i < cardIds.length; i++) {
+            uint256 id = cardIds[i];
+            if (totalMinted[id] < maxSupply[id]) {
+                return id;
+            }
+        }
+
+        return 0;
     }
 
     // Optional helper to see all card IDs
